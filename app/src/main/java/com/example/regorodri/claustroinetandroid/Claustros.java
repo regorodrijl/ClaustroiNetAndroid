dd925c81f5d03642c3eb406cd95b22514b60e3a1
regorodrijl <regorodrijl@gmail.com>
2016-08-12 13:52:40 +0200

    Initial commit

---
+++ app/src/main/java/com/example/regorodri/claustroinetandroid/Claustros.java
@@ -0,0 +1,13 @@
+package com.example.regorodri.claustroinetandroid;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Claustros extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_claustros);
+    }
+}
